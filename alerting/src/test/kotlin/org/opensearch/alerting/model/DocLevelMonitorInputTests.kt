b184af14cdbe93755b4fdca7b5bc79ca475e499f
AWSHurneyt <hurneyt@amazon.com>
2021-12-20 14:57:11 -0800

    Fixed faulty unit test.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/model/DocLevelMonitorInputTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/model/DocLevelMonitorInputTests.kt
@@ -26,6 +26,7 @@
 
 package org.opensearch.alerting.model
 
+import org.opensearch.alerting.model.action.Action
 import org.opensearch.alerting.model.docLevelInput.DocLevelMonitorInput
 import org.opensearch.alerting.model.docLevelInput.DocLevelQuery
 import org.opensearch.alerting.randomDocLevelMonitorInput
@@ -45,7 +46,9 @@ class DocLevelMonitorInputTests : OpenSearchTestCase() {
         assertEquals("Template args 'query' field does not match:", templateArgs[DocLevelQuery.QUERY_FIELD], query.query)
         assertEquals("Template args 'severity' field does not match:", templateArgs[DocLevelQuery.SEVERITY_FIELD], query.severity)
         assertEquals("Template args 'tags' field does not match:", templateArgs[DocLevelQuery.TAGS_FIELD], query.tags)
-        assertEquals("Template args 'actions' field does not match:", templateArgs[DocLevelQuery.ACTIONS_FIELD], query.actions)
+
+        val expectedActions = query.actions.map { mapOf(Action.NAME_FIELD to it.name) }
+        assertEquals("Template args 'actions' field does not match:", templateArgs[DocLevelQuery.ACTIONS_FIELD], expectedActions)
     }
 
     fun `testing DocLevelMonitorInput asTemplateArgs`() {
@@ -58,6 +61,9 @@ class DocLevelMonitorInputTests : OpenSearchTestCase() {
         // THEN
         assertEquals("Template args 'description' field does not match:", templateArgs[DocLevelMonitorInput.DESCRIPTION_FIELD], input.description)
         assertEquals("Template args 'indices' field does not match:", templateArgs[DocLevelMonitorInput.INDICES_FIELD], input.indices)
-        assertEquals("Template args 'queries' field does not match:", templateArgs[DocLevelMonitorInput.QUERIES_FIELD], input.queries)
+        assertEquals("Template args 'queries' field does not contain the expected number of queries:", input.queries.size, (templateArgs[DocLevelMonitorInput.QUERIES_FIELD] as List<*>).size)
+        input.queries.forEach {
+            assertTrue("Template args 'queries' field does not match:", (templateArgs[DocLevelMonitorInput.QUERIES_FIELD] as List<*>).contains(it.asTemplateArg()))
+        }
     }
 }
