73ae9672c234d786ceb9aebfaf679cf8409a7b0c
Mohammad Qureshi <47198598+qreshi@users.noreply.github.com>
2022-02-21 17:39:54 -0800

    Fix security tests with partial permissions (#307)
    Signed-off-by: Mohammad Qureshi <qreshi@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/resthandler/SecureMonitorRestApiIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/resthandler/SecureMonitorRestApiIT.kt
@@ -492,7 +492,7 @@ class SecureMonitorRestApiIT : AlertingRestTestCase() {
             testIndex,
             "hr_role",
             "HR",
-            "{\"term\": { \"accessible\": true}}"
+            """{\"term\": { \"accessible\": true}}"""
         )
         createUserRolesMapping("alerting_full_access", arrayOf(user))
 
@@ -524,11 +524,14 @@ class SecureMonitorRestApiIT : AlertingRestTestCase() {
             return ctx.results[0].hits.hits.size() == 1
         """.trimIndent()
 
-        val trigger = randomQueryLevelTrigger(condition = Script(triggerScript))
-        val monitor = randomQueryLevelMonitor(inputs = listOf(input), triggers = listOf(trigger))
+        val trigger = randomQueryLevelTrigger(condition = Script(triggerScript)).copy(actions = listOf())
+        val monitor = createMonitorWithClient(
+            userClient!!,
+            randomQueryLevelMonitor(inputs = listOf(input), triggers = listOf(trigger))
+        )
 
         try {
-            executeMonitor(monitor.id, params = DRYRUN_MONITOR)
+            executeMonitor(monitor.id)
             val alerts = searchAlerts(monitor)
             assertEquals("Incorrect number of alerts", 1, alerts.size)
         } finally {
@@ -547,7 +550,7 @@ class SecureMonitorRestApiIT : AlertingRestTestCase() {
             testIndex,
             "hr_role",
             "HR",
-            "{\"term\": { \"accessible\": true}}"
+            """{\"term\": { \"accessible\": true}}"""
         )
         createUserRolesMapping("alerting_full_access", arrayOf(user))
 
@@ -593,12 +596,16 @@ class SecureMonitorRestApiIT : AlertingRestTestCase() {
                 script = Script(triggerScript),
                 parentBucketPath = "composite_agg",
                 filter = null
-            )
+            ),
+            actions = listOf()
         )
-        val monitor = createMonitor(randomBucketLevelMonitor(inputs = listOf(input), enabled = false, triggers = listOf(trigger)))
+        val monitor = createMonitorWithClient(
+            userClient!!,
+            randomBucketLevelMonitor(inputs = listOf(input), enabled = false, triggers = listOf(trigger))
+        )
 
         try {
-            executeMonitor(monitor.id, params = DRYRUN_MONITOR)
+            executeMonitor(monitor.id)
             val alerts = searchAlerts(monitor)
             assertEquals("Incorrect number of alerts", 1, alerts.size)
         } finally {
