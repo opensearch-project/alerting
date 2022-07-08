dc93ffaf056ce9432735fbde131799d58c6c8438
Richard <richfu@amazon.com>
2022-07-08 15:35:08 -0700

    PLS WORK
    Signed-off-by: Richard <richfu@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
@@ -1,9 +1,7 @@
 package org.opensearch.alerting.util
 
 import org.apache.logging.log4j.LogManager
-import org.opensearch.action.support.WriteRequest
-import org.opensearch.alerting.action.IndexMonitorAction
-import org.opensearch.alerting.action.IndexMonitorRequest
+import org.opensearch.alerting.action.IndexMonitorResponse
 import org.opensearch.alerting.core.model.CronSchedule
 import org.opensearch.alerting.model.Monitor
 import org.opensearch.client.Client
@@ -13,10 +11,7 @@ import org.opensearch.cluster.service.ClusterService
 import org.opensearch.common.component.LifecycleListener
 import org.opensearch.common.settings.Setting
 import org.opensearch.common.unit.TimeValue
-import org.opensearch.common.xcontent.ToXContent
-import org.opensearch.common.xcontent.XContentBuilder
-import org.opensearch.common.xcontent.XContentType
-import org.opensearch.rest.RestRequest
+import org.opensearch.rest.RestStatus
 import org.opensearch.threadpool.ThreadPool
 import java.time.Instant
 import java.time.ZoneId
@@ -55,7 +50,7 @@ class ClusterMetricsVisualizationIndex(
         val cronSchedule = CronSchedule("*/15 * * * *", ZoneId.of("US/Pacific"))
         val monitor = Monitor(
             id = "123",
-            version = 0L,
+            version = 1L,
             name = "yepclock",
             enabled = true,
             user = null,
@@ -68,16 +63,7 @@ class ClusterMetricsVisualizationIndex(
             triggers = mutableListOf(),
             uiMetadata = mutableMapOf()
         )
-        val monitorRequest = IndexMonitorRequest(
-            monitorId = monitor.id,
-            seqNo = 0L,
-            primaryTerm = 0L,
-            refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE,
-            RestRequest.Method.POST,
-            monitor
-        )
-        val response = client.execute(IndexMonitorAction.INSTANCE, monitorRequest).get()
-        response.toXContent(XContentBuilder.builder(XContentType.JSON.xContent()), ToXContent.EMPTY_PARAMS)
+        IndexMonitorResponse("1234", 1L, 2L, 0L, RestStatus.OK, monitor)
     }
 
 //    init {
