20e763a5f07d7a9922407f14f916dcc28c3220a7
Richard <richfu@amazon.com>
2022-07-08 15:43:59 -0700

    PLS WORK
    Signed-off-by: Richard <richfu@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
@@ -1,7 +1,9 @@
 package org.opensearch.alerting.util
 
 import org.apache.logging.log4j.LogManager
-import org.opensearch.alerting.action.IndexMonitorResponse
+import org.opensearch.action.support.WriteRequest
+import org.opensearch.alerting.action.IndexMonitorAction
+import org.opensearch.alerting.action.IndexMonitorRequest
 import org.opensearch.alerting.core.model.CronSchedule
 import org.opensearch.alerting.model.Monitor
 import org.opensearch.client.Client
@@ -11,7 +13,10 @@ import org.opensearch.cluster.service.ClusterService
 import org.opensearch.common.component.LifecycleListener
 import org.opensearch.common.settings.Setting
 import org.opensearch.common.unit.TimeValue
-import org.opensearch.rest.RestStatus
+import org.opensearch.common.xcontent.ToXContent
+import org.opensearch.common.xcontent.XContentBuilder
+import org.opensearch.common.xcontent.XContentType
+import org.opensearch.rest.RestRequest
 import org.opensearch.threadpool.ThreadPool
 import java.time.Instant
 import java.time.ZoneId
@@ -63,7 +68,16 @@ class ClusterMetricsVisualizationIndex(
             triggers = mutableListOf(),
             uiMetadata = mutableMapOf()
         )
-        IndexMonitorResponse("1234", 1L, 2L, 0L, RestStatus.OK, monitor)
+        val monitorRequest = IndexMonitorRequest(
+            monitorId = "1234",
+            seqNo = 1L,
+            primaryTerm = 1L,
+            refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE,
+            RestRequest.Method.POST,
+            monitor
+        )
+        val response = client.execute(IndexMonitorAction.INSTANCE, monitorRequest).get()
+        response.toXContent(XContentBuilder.builder(XContentType.JSON.xContent()), ToXContent.EMPTY_PARAMS)
     }
 
 //    init {
