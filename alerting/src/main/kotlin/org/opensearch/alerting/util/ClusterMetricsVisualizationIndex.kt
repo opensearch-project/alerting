431ef47802c028b98625111d10e445fac3c5aebf
Richard <richfu@amazon.com>
2022-07-08 18:43:54 -0700

    PLS WORK
    Signed-off-by: Richard <richfu@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
@@ -67,7 +67,7 @@ class ClusterMetricsVisualizationIndex(
             uiMetadata = mutableMapOf()
         )
         val monitorRequest = IndexMonitorRequest(
-            monitorId = "69420",
+            monitorId = "12345",
             seqNo = 0L,
             primaryTerm = 0L,
             refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE,
