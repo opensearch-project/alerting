a6e5940d25b938059964f0d3d373816823c16207
Richard <richfu@amazon.com>
2022-07-11 09:46:46 -0700

    PLS WORK
    Signed-off-by: Richard <richfu@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/AlertingPlugin.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/AlertingPlugin.kt
@@ -241,6 +241,7 @@ internal class AlertingPlugin : PainlessExtension, ActionPlugin, ScriptPlugin, R
         docLevelMonitorQueries = DocLevelMonitorQueries(client, clusterService)
         scheduler = JobScheduler(threadPool, runner)
         sweeper = JobSweeper(environment.settings(), client, clusterService, threadPool, xContentRegistry, scheduler, ALERTING_JOB_TYPES)
+        clusterMetricsVisualization = ClusterMetricsVisualizationIndex(client, clusterService, threadPool)
         destinationMigrationCoordinator = DestinationMigrationCoordinator(client, clusterService, threadPool, scheduledJobIndices)
         // Create Monitor using Monitor.kt, and then feed the request to client in some way
         this.threadPool = threadPool
@@ -254,7 +255,8 @@ internal class AlertingPlugin : PainlessExtension, ActionPlugin, ScriptPlugin, R
             runner,
             scheduledJobIndices,
             docLevelMonitorQueries,
-            destinationMigrationCoordinator
+            destinationMigrationCoordinator,
+            clusterMetricsVisualization
         )
     }
 
