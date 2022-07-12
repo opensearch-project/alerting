dce2f227c54d2cb3444a4639c16abeb17ac07dc8
Richard <richfu@amazon.com>
2022-07-12 16:04:57 -0700

    PLS WORK
    Signed-off-by: Richard <richfu@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/util/ClusterMetricsVisualizationIndex.kt
@@ -5,13 +5,12 @@ import org.opensearch.action.admin.indices.create.CreateIndexRequest
 import org.opensearch.action.admin.indices.create.CreateIndexResponse
 import org.opensearch.alerting.opensearchapi.suspendUntil
 import org.opensearch.client.Client
-import org.opensearch.cluster.ClusterChangedEvent
-import org.opensearch.cluster.ClusterStateListener
 import org.opensearch.cluster.service.ClusterService
-import org.opensearch.common.component.LifecycleListener
 import org.opensearch.common.settings.Setting
 import org.opensearch.common.settings.Settings
+import org.opensearch.common.unit.TimeValue
 import org.opensearch.threadpool.ThreadPool
+import java.util.concurrent.TimeUnit
 
 private val log = LogManager.getLogger(ClusterMetricsVisualizationIndex::class.java)
 
@@ -19,12 +18,18 @@ class ClusterMetricsVisualizationIndex(
     private val client: Client,
     private val clusterService: ClusterService,
     private val threadPool: ThreadPool
-) : ClusterStateListener, LifecycleListener() {
+) {
 
     companion object {
         /** The index name pattern for all cluster metric visualizations indices */
         val CLUSTER_METRIC_VISUALIZATION_INDEX = ".opendistro-alerting-cluster-metrics"
         val METRICS_HISTORY_ENABLED = Setting.boolSetting("opendistro.alerting.metrics_history_enabled", true)
+        val METRICS_STORE_TIME = Setting.positiveTimeSetting(
+            "opendistro.alerting.metrics_history_max_age",
+            TimeValue(7, TimeUnit.DAYS),
+            Setting.Property.NodeScope, Setting.Property.Dynamic
+        )
+
         @JvmStatic
         fun clusterMetricsVisualizationsMappings(): String {
             return ClusterMetricsVisualizationIndex::class.java.classLoader.getResource("mappings/metrics-visualizations.json").readText()
@@ -46,64 +51,4 @@ class ClusterMetricsVisualizationIndex(
             return clusterState.routingTable.hasIndex(CLUSTER_METRIC_VISUALIZATION_INDEX)
         }
     }
-
-    init {
-        clusterService.addListener(this)
-        clusterService.addLifecycleListener(this)
-    }
-    override fun clusterChanged(p0: ClusterChangedEvent) {
-        log.info("THIS CLASS IS BEING CALLED")
-        // threadPool.schedule({ helper() }, TimeValue.timeValueMinutes(1), ThreadPool.Names.MANAGEMENT)
-    }
-
-//    suspend fun helper() {
-//        val cronSchedule = CronSchedule("*/15 * * * *", ZoneId.of("US/Pacific"))
-//        // index is already being created error, maybe check index is created and THEN run?
-//        val monitor = Monitor(
-//            id = "12345",
-//            version = 1L,
-//            name = "test_pls_work",
-//            enabled = true,
-//            user = null,
-//            schedule = IntervalSchedule(interval = 15, unit = ChronoUnit.MINUTES),
-//            lastUpdateTime = Instant.now().truncatedTo(ChronoUnit.MILLIS),
-//            enabledTime = Instant.now().truncatedTo(ChronoUnit.MILLIS),
-//            monitorType = Monitor.MonitorType.CLUSTER_METRICS_MONITOR,
-//            inputs = mutableListOf(),
-//            triggers = mutableListOf(),
-//            uiMetadata = mapOf()
-//        )
-//        val monitorRequest = IndexMonitorRequest(
-//            monitorId = "111111",
-//            seqNo = 1L,
-//            primaryTerm = 2L,
-//            refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE,
-//            RestRequest.Method.POST,
-//            monitor
-//        )
-//        val response = client.execute(IndexMonitorAction.INSTANCE, monitorRequest).get()
-//        log.info(" $response")
-//        // response.toXContent(XContentBuilder.builder(XContentType.JSON.xContent()), ToXContent.EMPTY_PARAMS)
-//    }
-
-//    init {
-//        if (!clusterMetricsVisualizationIndexExists()) {
-//            val indexRequest = CreateIndexRequest(CLUSTER_METRIC_VISUALIZATION_INDEX)
-//                .mapping(clusterMetricsVisualizationsMappings())
-//                .settings(
-//                    Settings.builder().put("index.hidden", true)
-//                        .build()
-//                )
-//            val createIndexResponse: CreateIndexResponse = client.admin().indices().create(indexRequest).get()
-//            createIndexResponse.isAcknowledged
-//        }
-//        clusterService.addListener(this)
-//        clusterService.clusterSettings.addSettingsUpdateConsumer(AlertingSettings.ALERT_HISTORY_ENABLED) { alertHistoryEnabled = it }
-//        clusterService.clusterSettings.addSettingsUpdateConsumer(AlertingSettings.ALERT_HISTORY_MAX_DOCS) { alertHistoryMaxDocs = it }
-//        clusterService.clusterSettings.addSettingsUpdateConsumer(AlertingSettings.ALERT_HISTORY_INDEX_MAX_AGE) { alertHistoryMaxAge = it }
-//        clusterService.clusterSettings.addSettingsUpdateConsumer(AlertingSettings.ALERT_HISTORY_ROLLOVER_PERIOD) {
-//            alertHistoryRolloverPeriod = it
-//            rescheduleAlertRollover()
-//        }
-//    }
 }
