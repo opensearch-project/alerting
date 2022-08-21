044a4bc335f02bfefef35b270d2036612d53cf01
jiahe zhang <zhanncha@amazon.com>
2022-08-21 11:43:30 -0700

    testing
    Signed-off-by: jiahe zhang <zhanncha@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/settings/AlertingSettings.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/settings/AlertingSettings.kt
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.LogManager
 import org.opensearch.action.search.SearchRequest
 import org.opensearch.action.search.SearchResponse
 import org.opensearch.alerting.AlertingPlugin
+import org.opensearch.alerting.alerts.AlertIndices
 import org.opensearch.alerting.core.model.ScheduledJob
 import org.opensearch.alerting.model.Monitor
 import org.opensearch.alerting.model.Trigger
@@ -335,6 +336,7 @@ class AlertingSettings(val client: Client) {
 
             val searchRequest = SearchRequest()
                 .source(searchSourceBuilder)
+                .indices(AlertIndices.ALL_FINDING_INDEX_PATTERN)
                 .indices(ScheduledJob.SCHEDULED_JOBS_INDEX)
             val response: SearchResponse = client.suspendUntil { client.search(searchRequest, it) }
             logger.info("Testing14Monitors response=> status: {${response.status()}} \n hits: {${response.hits.totalHits?.value}}")
