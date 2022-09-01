2fc4f8165490570be206ef3f5612f69e94112235
Surya Sashank Nistala <snistala@amazon.com>
2022-09-01 16:52:53 -0700

    add test to verify custom field mappings in monitor configs
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/MonitorDataSourcesIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/MonitorDataSourcesIT.kt
@@ -96,6 +96,36 @@ class MonitorDataSourcesIT : AlertingSingleNodeTestCase() {
         searchAlerts(id)
     }
 
+    fun `test execute monitor with custom query index and custom field mappings`() {
+        val docQuery = DocLevelQuery(query = "test_field:\"us-west-2\"", name = "3")
+        val docLevelInput = DocLevelMonitorInput("description", listOf(index), listOf(docQuery))
+        val trigger = randomDocumentLevelTrigger(condition = ALWAYS_RUN)
+        val customQueryIndex = "custom_alerts_index"
+        var monitor = randomDocumentLevelMonitor(
+            inputs = listOf(docLevelInput),
+            triggers = listOf(trigger),
+            dataSources = DataSources(
+                queryIndex = customQueryIndex,
+                queryIndexMappingsByType = mapOf(Pair("text", mapOf(Pair("analyzer", "rule_analyzer")))),
+            )
+        )
+        val monitorResponse = createMonitor(monitor)
+        val testTime = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.now().truncatedTo(MILLIS))
+        val testDoc = """{
+            "message" : "This is an error from IAD region",
+            "test_strict_date_time" : "$testTime",
+            "test_field" : "us-west-2"
+        }"""
+        assertFalse(monitorResponse?.id.isNullOrEmpty())
+        monitor = monitorResponse!!.monitor
+        indexDoc(index, "1", testDoc)
+        val id = monitorResponse.id
+        val executeMonitorResponse = executeMonitor(monitor, id, false)
+        Assert.assertEquals(executeMonitorResponse!!.monitorRunResult.monitorName, monitor.name)
+        Assert.assertEquals(executeMonitorResponse.monitorRunResult.triggerResults.size, 1)
+        searchAlerts(id)
+    }
+
     fun `test execute monitor with custom findings index`() {
         val docQuery = DocLevelQuery(query = "test_field:\"us-west-2\"", name = "3")
         val docLevelInput = DocLevelMonitorInput("description", listOf(index), listOf(docQuery))
