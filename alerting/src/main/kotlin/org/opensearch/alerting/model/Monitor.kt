050f42a4e08c1470521609ffb7f7599a0ce544ba
Dennis Toepker <toepkerd@amazon.com>
2022-09-01 20:35:40 -0700

    draft refactor of REST handling and Transport, still work in progress
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/model/Monitor.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/model/Monitor.kt
@@ -322,7 +322,7 @@ data class Monitor(
 
         @JvmStatic
         @Throws(IOException::class)
-        fun readFrom(sin: StreamInput): Monitor? {
+        fun readFrom(sin: StreamInput): Monitor {
             return Monitor(sin)
         }
 
---
+++ alerting/src/main/kotlin/org/opensearch/alerting/rules/util/RuleExecutor.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.rules.util
+
+object RuleExecutor {
+    fun getSuggestions(obj: Any): List<String> {
+//        val ruleFactory = RuleFactory()
+//        val suggestions = mutableListOf<String>()
+//        for (registeredRule in RuleFactory.RegisteredRule.values()) {
+//            val rule = ruleFactory.getRule(registeredRule)
+//            val passedCheck = rule.evaluate(monitor)
+//            if (!passedCheck) {
+//                suggestions.add(rule.suggestion())
+//            }
+//        }
+        return listOf("placeholder suggestion")
+    }
+}
