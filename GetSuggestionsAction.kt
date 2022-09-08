532cac98809b66f2ba6de8f1dfa0787361ac35d6
Dennis Toepker <toepkerd@amazon.com>
2022-09-07 17:46:38 -0700

    re-adding getsuggestionsaction
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

---
+++ GetSuggestionsAction.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.action
+
+import org.opensearch.action.ActionType
+
+class GetSuggestionsAction private constructor() : ActionType<GetSuggestionsResponse>(NAME, ::GetSuggestionsResponse) {
+    companion object {
+        val INSTANCE = GetSuggestionsAction()
+        const val NAME = "cluster:admin/opensearch/suggestions/get"
+    }
+}
