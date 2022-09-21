415dc821f1280bf64525e137087286dbc219bcc9
Surya Sashank Nistala <snistala@amazon.com>
2022-09-20 18:19:32 -0700

    Adds support for fetching alerts from custom alert index in Get Alerts transport layer (#561)
    * adds support for fetching alerts from custom alert index in Get Alerts transport layer

--- alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetAlertsAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetAlertsAction.kt
@@ -5,6 +5,10 @@
 
 package org.opensearch.alerting.transport
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.apache.logging.log4j.LogManager
 import org.opensearch.action.ActionListener
 import org.opensearch.action.search.SearchRequest
@@ -14,9 +18,13 @@ import org.opensearch.action.support.HandledTransportAction
 import org.opensearch.alerting.action.GetAlertsAction
 import org.opensearch.alerting.action.GetAlertsRequest
 import org.opensearch.alerting.action.GetAlertsResponse
+import org.opensearch.alerting.action.GetMonitorAction
+import org.opensearch.alerting.action.GetMonitorRequest
+import org.opensearch.alerting.action.GetMonitorResponse
 import org.opensearch.alerting.alerts.AlertIndices
 import org.opensearch.alerting.model.Alert
 import org.opensearch.alerting.opensearchapi.addFilter
+import org.opensearch.alerting.opensearchapi.suspendUntil
 import org.opensearch.alerting.settings.AlertingSettings
 import org.opensearch.alerting.util.AlertingException
 import org.opensearch.client.Client
@@ -32,7 +40,9 @@ import org.opensearch.common.xcontent.XContentType
 import org.opensearch.commons.authuser.User
 import org.opensearch.index.query.Operator
 import org.opensearch.index.query.QueryBuilders
+import org.opensearch.rest.RestRequest
 import org.opensearch.search.builder.SearchSourceBuilder
+import org.opensearch.search.fetch.subphase.FetchSourceContext
 import org.opensearch.search.sort.SortBuilders
 import org.opensearch.search.sort.SortOrder
 import org.opensearch.tasks.Task
@@ -40,6 +50,7 @@ import org.opensearch.transport.TransportService
 import java.io.IOException
 
 private val log = LogManager.getLogger(TransportGetAlertsAction::class.java)
+private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 
 class TransportGetAlertsAction @Inject constructor(
     transportService: TransportService,
@@ -47,7 +58,8 @@ class TransportGetAlertsAction @Inject constructor(
     clusterService: ClusterService,
     actionFilters: ActionFilters,
     val settings: Settings,
-    val xContentRegistry: NamedXContentRegistry
+    val xContentRegistry: NamedXContentRegistry,
+    val transportGetMonitorAction: TransportGetMonitorAction
 ) : HandledTransportAction<GetAlertsRequest, GetAlertsResponse>(
     GetAlertsAction.NAME, transportService, actionFilters, ::GetAlertsRequest
 ),
@@ -85,7 +97,6 @@ class TransportGetAlertsAction @Inject constructor(
         if (getAlertsRequest.monitorId != null) {
             queryBuilder.filter(QueryBuilders.termQuery("monitor_id", getAlertsRequest.monitorId))
         }
-
         if (!tableProp.searchString.isNullOrBlank()) {
             queryBuilder
                 .must(
@@ -105,11 +116,52 @@ class TransportGetAlertsAction @Inject constructor(
             .from(tableProp.startIndex)
 
         client.threadPool().threadContext.stashContext().use {
-            resolve(searchSourceBuilder, actionListener, user)
+            scope.launch {
+                try {
+                    val alertIndex = resolveAlertsIndexName(getAlertsRequest)
+                    getAlerts(alertIndex, searchSourceBuilder, actionListener, user)
+                } catch (t: Exception) {
+                    log.error("Failed to get alerts", t)
+                    if (t is AlertingException) {
+                        actionListener.onFailure(t)
+                    } else {
+                        actionListener.onFailure(AlertingException.wrap(t))
+                    }
+                }
+            }
         }
     }
 
-    fun resolve(
+    /** Precedence order for resolving alert index to be queried:
+     1. alertIndex param.
+     2. alert index mentioned in monitor data sources.
+     3. Default alert indices pattern
+     */
+    suspend fun resolveAlertsIndexName(getAlertsRequest: GetAlertsRequest): String {
+        var alertIndex = AlertIndices.ALL_ALERT_INDEX_PATTERN
+        if (getAlertsRequest.alertIndex.isNullOrEmpty() == false) {
+            alertIndex = getAlertsRequest.alertIndex
+        } else if (getAlertsRequest.monitorId.isNullOrEmpty() == false)
+            withContext(Dispatchers.IO) {
+                val getMonitorRequest = GetMonitorRequest(
+                    getAlertsRequest.monitorId,
+                    -3L,
+                    RestRequest.Method.GET,
+                    FetchSourceContext.FETCH_SOURCE
+                )
+                val getMonitorResponse: GetMonitorResponse =
+                    transportGetMonitorAction.client.suspendUntil {
+                        execute(GetMonitorAction.INSTANCE, getMonitorRequest, it)
+                    }
+                if (getMonitorResponse.monitor != null) {
+                    alertIndex = getMonitorResponse.monitor!!.dataSources.alertsIndex
+                }
+            }
+        return alertIndex
+    }
+
+    fun getAlerts(
+        alertIndex: String,
         searchSourceBuilder: SearchSourceBuilder,
         actionListener: ActionListener<GetAlertsResponse>,
         user: User?
@@ -117,25 +169,25 @@ class TransportGetAlertsAction @Inject constructor(
         // user is null when: 1/ security is disabled. 2/when user is super-admin.
         if (user == null) {
             // user is null when: 1/ security is disabled. 2/when user is super-admin.
-            search(searchSourceBuilder, actionListener)
+            search(alertIndex, searchSourceBuilder, actionListener)
         } else if (!doFilterForUser(user)) {
             // security is enabled and filterby is disabled.
-            search(searchSourceBuilder, actionListener)
+            search(alertIndex, searchSourceBuilder, actionListener)
         } else {
             // security is enabled and filterby is enabled.
             try {
                 log.info("Filtering result by: ${user.backendRoles}")
                 addFilter(user, searchSourceBuilder, "monitor_user.backend_roles.keyword")
-                search(searchSourceBuilder, actionListener)
+                search(alertIndex, searchSourceBuilder, actionListener)
             } catch (ex: IOException) {
                 actionListener.onFailure(AlertingException.wrap(ex))
             }
         }
     }
 
-    fun search(searchSourceBuilder: SearchSourceBuilder, actionListener: ActionListener<GetAlertsResponse>) {
+    fun search(alertIndex: String, searchSourceBuilder: SearchSourceBuilder, actionListener: ActionListener<GetAlertsResponse>) {
         val searchRequest = SearchRequest()
-            .indices(AlertIndices.ALL_ALERT_INDEX_PATTERN)
+            .indices(alertIndex)
             .source(searchSourceBuilder)
 
         client.search(
