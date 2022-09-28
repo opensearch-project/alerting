8fb4e100735604ae4f5b1debce12de07d05ff21e
Dennis Toepker <toepkerd@amazon.com>
2022-09-28 16:48:29 -0700

    fixed compiler errors in SuggestionsTest.kt
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/model/SuggestionsTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/model/SuggestionsTests.kt
@@ -8,6 +8,7 @@ package org.opensearch.alerting.model
 import org.apache.logging.log4j.LogManager
 import org.opensearch.alerting.core.model.SearchInput
 import org.opensearch.alerting.model.suggestions.rules.QueryWithAggsRule
+import org.opensearch.alerting.model.suggestions.rules.util.ComponentType
 import org.opensearch.alerting.model.suggestions.rules.util.RuleExecutor
 import org.opensearch.alerting.randomBucketLevelMonitor
 import org.opensearch.alerting.randomQueryLevelMonitor
@@ -21,7 +22,7 @@ private val log = LogManager.getLogger(SuggestionsTests::class.java)
 class SuggestionsTests : OpenSearchTestCase() {
     fun `test no suggestions found on valid object and component`() {
         // ensure framework returns clean response when there are no Rules that match a valid input's profile
-        val suggestions = RuleExecutor.getSuggestions(randomQueryLevelMonitor(), "monitor.name")
+        val suggestions = RuleExecutor.getSuggestions(randomQueryLevelMonitor(), ComponentType.NOT_SUPPORTED_COMPONENT)
         assertEquals(1, suggestions.size)
         assertEquals(suggestions[0], RuleExecutor.NO_SUGGESTIONS_FOUND)
     }
@@ -33,13 +34,13 @@ class SuggestionsTests : OpenSearchTestCase() {
     // QueryWithAggsRule tests
     fun `test QueryWithAggsRule gives no suggestion if monitor isn't query-level`() {
         val monitor = randomBucketLevelMonitor()
-        val suggestions = RuleExecutor.getSuggestions(monitor, "monitor")
+        val suggestions = RuleExecutor.getSuggestions(monitor, ComponentType.MONITOR_WHOLE)
         assert(!suggestions.contains(QueryWithAggsRule.QUERY_WITH_AGGS_SUGGESTION))
     }
 
     fun `test QueryWithAggsRule gives no suggestion if query level monitor's input doesn't use aggregations`() {
         val monitor = randomQueryLevelMonitor() // the random monitor returned has, by default, a single SearchInput that has null aggregations
-        val suggestions = RuleExecutor.getSuggestions(monitor, "monitor")
+        val suggestions = RuleExecutor.getSuggestions(monitor, ComponentType.MONITOR_WHOLE)
         assert(!suggestions.contains(QueryWithAggsRule.QUERY_WITH_AGGS_SUGGESTION))
     }
 
@@ -49,7 +50,7 @@ class SuggestionsTests : OpenSearchTestCase() {
                 SearchInput(emptyList(), SearchSourceBuilder().query(QueryBuilders.matchAllQuery()).aggregation(AggregationBuilders.max("max").field("field1")))
             )
         )
-        val suggestions = RuleExecutor.getSuggestions(monitor, "monitor")
+        val suggestions = RuleExecutor.getSuggestions(monitor, ComponentType.MONITOR_WHOLE)
         assert(suggestions.contains(QueryWithAggsRule.QUERY_WITH_AGGS_SUGGESTION))
     }
 }
