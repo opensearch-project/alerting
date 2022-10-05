aaa271ca909f3c1fd0a1f4a40f1882c5e38bd6b7
jiahe zhang <zhanncha@amazon.com>
2022-10-05 13:34:55 -0700

    ktlint
    Signed-off-by: jiahe zhang <zhanncha@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/AlertingSettingsIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/AlertingSettingsIT.kt
@@ -10,14 +10,14 @@ import org.opensearch.alerting.settings.AlertingSettings
 class AlertingSettingsIT : AlertingRestTestCase() {
 
     fun `test updating setting of overall max actions with less actions than the current maximum value`() {
-            client().updateSettings(AlertingSettings.TOTAL_MAX_ACTIONS_ACROSS_TRIGGERS.key, -10)
+        client().updateSettings(AlertingSettings.TOTAL_MAX_ACTIONS_ACROSS_TRIGGERS.key, -10)
 
-            assert(MonitorRunnerService.monitorCtx.totalMaxActionsAcrossTriggers != -10)
+        assert(MonitorRunnerService.monitorCtx.totalMaxActionsAcrossTriggers != -10)
     }
 
     fun `test updating setting of max actions per trigger with more actions than the maximum allowed actions across triggers`() {
-            client().updateSettings(AlertingSettings.TOTAL_MAX_ACTIONS_PER_TRIGGER.key, 10)
+        client().updateSettings(AlertingSettings.TOTAL_MAX_ACTIONS_PER_TRIGGER.key, 10)
 
-            assert(MonitorRunnerService.monitorCtx.totalMaxActionsAcrossTriggers != 10)
+        assert(MonitorRunnerService.monitorCtx.totalMaxActionsAcrossTriggers != 10)
     }
 }
