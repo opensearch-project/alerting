252fe40dd7319c068e4deaa9e70bb5c15e32a1e3
Surya Sashank Nistala <snistala@amazon.com>
2022-10-06 13:06:37 -0700

    add test to verify get alerts with monitor list
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/GetAlertsIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/GetAlertsIT.kt
@@ -1,7 +1,6 @@
 package org.opensearch.alerting
 
 import org.junit.Assert
-import org.opensearch.action.search.SearchRequest
 import org.opensearch.alerting.action.GetAlertsAction
 import org.opensearch.alerting.action.GetAlertsRequest
 import org.opensearch.alerting.core.model.DocLevelMonitorInput
@@ -9,8 +8,6 @@ import org.opensearch.alerting.core.model.DocLevelQuery
 import org.opensearch.alerting.model.DataSources
 import org.opensearch.alerting.model.Table
 import org.opensearch.alerting.transport.AlertingSingleNodeTestCase
-import org.opensearch.index.query.BoolQueryBuilder
-import org.opensearch.index.query.QueryBuilders
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit.MILLIS
@@ -43,10 +40,6 @@ class GetAlertsIT : AlertingSingleNodeTestCase() {
         val alerts = searchAlerts(id, customAlertsIndex)
         assertEquals("Alert saved for test monitor", 1, alerts.size)
         val table = Table("asc", "id", null, 1, 0, "")
-        val sr = SearchRequest(customAlertsIndex)
-        val queryBuilder = BoolQueryBuilder()
-        queryBuilder.filter(QueryBuilders.termQuery("monitor_id", monitor))
-        sr.source().query(queryBuilder)
         var getAlertsResponse = client()
             .execute(GetAlertsAction.INSTANCE, GetAlertsRequest(table, "ALL", "ALL", null, customAlertsIndex))
             .get()
@@ -63,11 +56,9 @@ class GetAlertsIT : AlertingSingleNodeTestCase() {
         val docQuery = DocLevelQuery(query = "test_field:\"us-west-2\"", name = "3")
         val docLevelInput = DocLevelMonitorInput("description", listOf(index), listOf(docQuery))
         val trigger = randomDocumentLevelTrigger(condition = ALWAYS_RUN)
-        val customAlertsIndex = "custom_alerts_index"
         var monitor = randomDocumentLevelMonitor(
             inputs = listOf(docLevelInput),
             triggers = listOf(trigger),
-            dataSources = DataSources(alertsIndex = customAlertsIndex)
         )
         val monitorResponse = createMonitor(monitor)
         val testTime = DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.now().truncatedTo(MILLIS))
@@ -78,21 +69,41 @@ class GetAlertsIT : AlertingSingleNodeTestCase() {
         }"""
 
         monitor = monitorResponse!!.monitor
-        indexDoc(index, "1", testDoc)
-        val id = monitorResponse.id
-        executeMonitor(monitor, id, false)
 
-        val alerts = searchAlerts(id, customAlertsIndex)
+        val id = monitorResponse.id
+
+        var monitor1 = randomDocumentLevelMonitor(
+            inputs = listOf(docLevelInput),
+            triggers = listOf(trigger),
+        )
+        val monitorResponse1 = createMonitor(monitor1)
+        monitor1 = monitorResponse1!!.monitor
+        val id1 = monitorResponse1.id
+        indexDoc(index, "1", testDoc)
+        executeMonitor(monitor1, id1, false)
+        executeMonitor(monitor, id, false)
+        val alerts = searchAlerts(id)
         assertEquals("Alert saved for test monitor", 1, alerts.size)
-        val table = Table("asc", "id", null, 1, 0, "")
+        val alerts1 = searchAlerts(id)
+        assertEquals("Alert saved for test monitor", 1, alerts1.size)
+        val table = Table("asc", "id", null, 1000, 0, "")
         var getAlertsResponse = client()
             .execute(
                 GetAlertsAction.INSTANCE,
-                GetAlertsRequest(table, "ALL", "ALL", null, customAlertsIndex, listOf(id, "1", "2"))
+                GetAlertsRequest(table, "ALL", "ALL", null, null)
             )
             .get()
-        SearchRequest(customAlertsIndex)
+
         Assert.assertTrue(getAlertsResponse != null)
-        Assert.assertTrue(getAlertsResponse.alerts.size == 1)
+        Assert.assertTrue(getAlertsResponse.alerts.size == 2)
+
+        var alertsResponseForRequestWithoutCustomIndex = client()
+            .execute(
+                GetAlertsAction.INSTANCE,
+                GetAlertsRequest(table, "ALL", "ALL", null, null, listOf(id, id1, "1", "2"))
+            )
+            .get()
+        Assert.assertTrue(alertsResponseForRequestWithoutCustomIndex != null)
+        Assert.assertTrue(alertsResponseForRequestWithoutCustomIndex.alerts.size == 2)
     }
 }
