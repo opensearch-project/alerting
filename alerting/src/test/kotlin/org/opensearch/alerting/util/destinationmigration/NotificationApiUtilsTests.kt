e20cdbb12cf57e7c83616365ff72d56c6bb938d7
danielkyalo599 <johbiento@gmail.com>
2023-01-16 15:31:47 +0300

    Test issue 731 and 529
    Signed-off-by: danielkyalo599 <johbiento@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/util/destinationmigration/NotificationApiUtilsTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/util/destinationmigration/NotificationApiUtilsTests.kt
@@ -11,7 +11,7 @@ import org.opensearch.commons.notifications.model.NotificationConfig
 import org.opensearch.commons.notifications.model.NotificationConfigInfo
 import java.time.Instant
 
-class NotificationApiUtilsTests : LuceneTestCase(){
+class NotificationApiUtilsTests : LuceneTestCase() {
 
     fun testGetTitle() {
         // create the subject that we're going to test with
