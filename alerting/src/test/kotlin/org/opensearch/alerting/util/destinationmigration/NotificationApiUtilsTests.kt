cc776c84c8f06bef18999cefa8d7420c95935c83
danielkyalo599 <johbiento@gmail.com>
2023-01-16 17:35:25 +0300

    Test issue 731 and 529
    Signed-off-by: danielkyalo599 <johbiento@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/util/destinationmigration/NotificationApiUtilsTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/util/destinationmigration/NotificationApiUtilsTests.kt
@@ -4,22 +4,16 @@
  */
 
 package org.opensearch.alerting.util.destinationmigration
-
+import org.apache.lucene.tests.util.LuceneTestCase
 import org.junit.Assert
 import org.opensearch.commons.notifications.model.ConfigType
 import org.opensearch.commons.notifications.model.NotificationConfig
 import org.opensearch.commons.notifications.model.NotificationConfigInfo
-import org.opensearch.test.OpenSearchTestCase
 import java.time.Instant
 
-/*
-Tested issue 731 - Ensures correct subject line used in email/SNS notifications."
-and  "Tested issue 529 - Returns slack notifications to original formatting of $subject \n\n $message without hardcoded prefix
- */
+class NotificationApiUtilsTests : LuceneTestCase() {
 
-class NotificationApiUtilsTests : OpenSearchTestCase() {
-
-    fun `test getTitle`() {
+    fun testGetTitle() {
         // create the subject that we're going to test with
         val subject = "Urgent: Server down on production"
         // create the lastUpdatedTime and createdTime
@@ -47,7 +41,7 @@ class NotificationApiUtilsTests : OpenSearchTestCase() {
         val slackConfig = NotificationConfigInfo(testSlack, lastUpdatedTime, createdTime, slackNotificationConfig)
         val otherConfig = NotificationConfigInfo(others, lastUpdatedTime, createdTime, otherNotificationConfig)
 
-        // Test that the getTitle method returns the subject when called with email , sns or   Slack config types
+        // Test that the getTitle method returns the subject when called with email , sns or Slack config types
         Assert.assertEquals(subject, emailConfig.getTitle(subject))
         Assert.assertEquals(subject, snsConfig.getTitle(subject))
         Assert.assertEquals("Alerting-Notification Action", slackConfig.getTitle(subject))
