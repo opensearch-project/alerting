adb0e0510756a0bb2e04a999d39381f8dda3bb2f
danielkyalo599 <johbiento@gmail.com>
2023-01-18 22:28:58 +0300

    Send Notification via Telegram Feature
    Signed-off-by: danielkyalo599 <johbiento@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/model/DestinationTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/model/DestinationTests.kt
@@ -106,7 +106,7 @@ class DestinationTests : OpenSearchTestCase() {
     fun `test chime destination create using stream`() {
         val chimeDest = Destination(
             "1234", 0L, 1, 1, 1, DestinationType.CHIME, "TestChimeDest",
-            randomUser(), Instant.now(), Chime("test.com"), null, null, null,telegram = null,
+            randomUser(), Instant.now(), Chime("test.com"), null, null, null, telegram = null,
         )
 
         val out = BytesStreamOutput()
@@ -130,7 +130,7 @@ class DestinationTests : OpenSearchTestCase() {
     fun `test slack destination create using stream`() {
         val slackDest = Destination(
             "2345", 1L, 2, 1, 1, DestinationType.SLACK, "TestSlackDest",
-            randomUser(), Instant.now(), null, Slack("mytest.com"), null, null,telegram = null,
+            randomUser(), Instant.now(), null, Slack("mytest.com"), null, null, telegram = null,
         )
 
         val out = BytesStreamOutput()
