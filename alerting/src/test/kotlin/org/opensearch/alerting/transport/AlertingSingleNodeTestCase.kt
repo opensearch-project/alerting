702da92495481bebfc18dbee2de9544a4245759a
Petar Dzepina <petar.dzepina@gmail.com>
2023-04-06 20:24:41 +0200

    Fixed transformation of document field names before running percolator search (#845)
    * Fixed transformation of document field names before running percolator search
    
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/transport/AlertingSingleNodeTestCase.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/transport/AlertingSingleNodeTestCase.kt
@@ -38,6 +38,7 @@ import org.opensearch.commons.alerting.model.Table
 import org.opensearch.index.query.TermQueryBuilder
 import org.opensearch.index.reindex.ReindexModulePlugin
 import org.opensearch.index.seqno.SequenceNumbers
+import org.opensearch.join.ParentJoinModulePlugin
 import org.opensearch.plugins.Plugin
 import org.opensearch.rest.RestRequest
 import org.opensearch.search.builder.SearchSourceBuilder
@@ -229,7 +230,7 @@ abstract class AlertingSingleNodeTestCase : OpenSearchSingleNodeTestCase() {
     ).get()
 
     override fun getPlugins(): List<Class<out Plugin>> {
-        return listOf(AlertingPlugin::class.java, ReindexModulePlugin::class.java)
+        return listOf(AlertingPlugin::class.java, ReindexModulePlugin::class.java, ParentJoinModulePlugin::class.java)
     }
 
     override fun resetNodeAfterTest(): Boolean {
