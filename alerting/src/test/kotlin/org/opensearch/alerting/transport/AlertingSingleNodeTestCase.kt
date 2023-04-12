a82b0fa0b93ecceb652a50a5ce60911ed682fd43
Petar Dzepina <petar.dzepina@gmail.com>
2023-04-13 00:45:39 +0200

    fixed module class names
    Signed-off-by: Petar Dzepina <petar.dzepina@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/transport/AlertingSingleNodeTestCase.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/transport/AlertingSingleNodeTestCase.kt
@@ -36,9 +36,9 @@ import org.opensearch.commons.alerting.model.Finding
 import org.opensearch.commons.alerting.model.Monitor
 import org.opensearch.commons.alerting.model.Table
 import org.opensearch.index.query.TermQueryBuilder
-import org.opensearch.index.reindex.ReindexModulePlugin
+import org.opensearch.index.reindex.ReindexPlugin
 import org.opensearch.index.seqno.SequenceNumbers
-import org.opensearch.join.ParentJoinModulePlugin
+import org.opensearch.join.ParentJoinPlugin
 import org.opensearch.plugins.Plugin
 import org.opensearch.rest.RestRequest
 import org.opensearch.search.builder.SearchSourceBuilder
@@ -230,7 +230,7 @@ abstract class AlertingSingleNodeTestCase : OpenSearchSingleNodeTestCase() {
     ).get()
 
     override fun getPlugins(): List<Class<out Plugin>> {
-        return listOf(AlertingPlugin::class.java, ReindexModulePlugin::class.java, ParentJoinModulePlugin::class.java)
+        return listOf(AlertingPlugin::class.java, ReindexPlugin::class.java, ParentJoinPlugin::class.java)
     }
 
     override fun resetNodeAfterTest(): Boolean {
