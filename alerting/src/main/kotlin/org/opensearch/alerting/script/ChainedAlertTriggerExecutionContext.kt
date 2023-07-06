322248f4c26a6affa5a08b171145befc0585af58
Surya Sashank Nistala <snistala@amazon.com>
2023-07-06 15:19:02 -0700

    add license headers to files
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/script/ChainedAlertTriggerExecutionContext.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/script/ChainedAlertTriggerExecutionContext.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.alerting.script
 
 import org.opensearch.alerting.model.WorkflowRunResult
