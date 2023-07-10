9110056148d305722eea888e606ce46e6a4d6744
Surya Sashank Nistala <snistala@amazon.com>
2023-07-10 15:13:28 -0700

    fix acknowledge chained alert bug
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportAcknowledgeChainedAlertAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportAcknowledgeChainedAlertAction.kt
@@ -139,7 +139,7 @@ class TransportAcknowledgeChainedAlertAction @Inject constructor(
             try {
                 val queryBuilder = QueryBuilders.boolQuery()
                     .must(
-                        QueryBuilders.wildcardQuery("workflow_id", "request.workflowId")
+                        QueryBuilders.wildcardQuery("workflow_id", request.workflowId)
                     )
                     .must(QueryBuilders.termsQuery("_id", request.alertIds))
                 if (workflow.inputs.isEmpty() || (workflow.inputs[0] is CompositeInput) == false) {
