4a85d134506a8b30c2a02fadd846c10e6d129083
Subhobrata Dey <sbcd90@gmail.com>
2023-08-17 00:27:21 +0000

    optimize doc-level monitor workflow for index patterns
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/util/IndexUtils.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/util/IndexUtils.kt
@@ -21,7 +21,6 @@ import org.opensearch.commons.alerting.util.IndexUtils
 import org.opensearch.core.action.ActionListener
 import org.opensearch.core.xcontent.NamedXContentRegistry
 import org.opensearch.core.xcontent.XContentParser
-import org.opensearch.index.IndexNotFoundException
 
 class IndexUtils {
 
@@ -149,10 +148,6 @@ class IndexUtils {
                     index
                 )
                 result.addAll(concreteIndices)
-            }
-
-            if (result.size == 0) {
-                throw IndexNotFoundException(indices[0])
             }
 
             return result
