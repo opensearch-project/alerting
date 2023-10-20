eaae8e631b0d6dbc5931e7c2dedf49de5dbd9181
Jacob Choi <junhyungch@gmail.com>
2023-10-20 17:05:16 -0400

    fixing param mismatch
    Signed-off-by: Jacob Choi <junhyungch@gmail.com>

--- alerting/src/test/kotlin/org/opensearch/alerting/resthandler/MonitorRestApiIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/resthandler/MonitorRestApiIT.kt
@@ -821,8 +821,9 @@ class MonitorRestApiIT : AlertingRestTestCase() {
 
         // Wait 5 seconds for event to be processed and alerts moved
         OpenSearchTestCase.waitUntil({
+            val alerts = searchAlerts(monitor)
             val historyAlerts = searchAlerts(monitor, AlertIndices.ALERT_HISTORY_WRITE_INDEX)
-            return@waitUntil (historyAlerts.size == 1)
+            return@waitUntil (alerts.isEmpty() && historyAlerts.size == 1)
         }, 5, TimeUnit.SECONDS)
 
         val alerts = searchAlerts(monitor)
@@ -855,8 +856,7 @@ class MonitorRestApiIT : AlertingRestTestCase() {
 
         // Wait 5 seconds for event to be processed and alerts moved
         OpenSearchTestCase.waitUntil({
-            val historyAlerts = searchAlerts(monitor, AlertIndices.ALERT_HISTORY_WRITE_INDEX)
-            return@waitUntil (historyAlerts.size == 1)
+            return@waitUntil false
         }, 5, TimeUnit.SECONDS)
 
         val alerts = searchAlerts(monitor)
