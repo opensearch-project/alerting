b69bb5639263832429dbcb311fcb57d403ff8a0e
Megha Goyal <56077967+goyamegh@users.noreply.github.com>
2024-02-06 11:41:03 -0800

    Bulk index findings and sequentially invoke auto-correlations (#1355)
    * Bulk index findings and sequentially invoke auto-correlations
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>
    
    * Bulk index findings in batches of 10000 and make it configurable
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>
    
    * Addressing review comments
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>
    
    * Add integ tests to test bulk index findings
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>
    
    * Fix ktlint formatting
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>
    
    ---------
    
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/MonitorRunnerService.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/MonitorRunnerService.kt
@@ -23,8 +23,10 @@ import org.opensearch.alerting.model.WorkflowRunResult
 import org.opensearch.alerting.model.destination.DestinationContextFactory
 import org.opensearch.alerting.opensearchapi.retry
 import org.opensearch.alerting.script.TriggerExecutionContext
+import org.opensearch.alerting.settings.AlertingSettings
 import org.opensearch.alerting.settings.AlertingSettings.Companion.ALERT_BACKOFF_COUNT
 import org.opensearch.alerting.settings.AlertingSettings.Companion.ALERT_BACKOFF_MILLIS
+import org.opensearch.alerting.settings.AlertingSettings.Companion.FINDINGS_INDEXING_BATCH_SIZE
 import org.opensearch.alerting.settings.AlertingSettings.Companion.INDEX_TIMEOUT
 import org.opensearch.alerting.settings.AlertingSettings.Companion.MAX_ACTIONABLE_ALERT_COUNT
 import org.opensearch.alerting.settings.AlertingSettings.Companion.MOVE_ALERTS_BACKOFF_COUNT
@@ -168,6 +170,11 @@ object MonitorRunnerService : JobRunner, CoroutineScope, AbstractLifecycleCompon
         }
 
         monitorCtx.indexTimeout = INDEX_TIMEOUT.get(monitorCtx.settings)
+
+        monitorCtx.findingsIndexBatchSize = FINDINGS_INDEXING_BATCH_SIZE.get(monitorCtx.settings)
+        monitorCtx.clusterService!!.clusterSettings.addSettingsUpdateConsumer(AlertingSettings.FINDINGS_INDEXING_BATCH_SIZE) {
+            monitorCtx.findingsIndexBatchSize = it
+        }
 
         return this
     }
