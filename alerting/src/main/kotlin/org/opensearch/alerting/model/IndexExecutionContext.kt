d10cd9dee789e348434cc3ff6c806e46a0cdbc50
Surya Sashank Nistala <snistala@amazon.com>
2024-02-27 13:09:10 -0800

    add javadoc
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/model/IndexExecutionContext.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/model/IndexExecutionContext.kt
@@ -7,6 +7,7 @@ package org.opensearch.alerting.model
 
 import org.opensearch.commons.alerting.model.DocLevelQuery
 
+/** DTO that contains all the necessary context for fetching data from shard and performing percolate queries */
 data class IndexExecutionContext(
     val queries: List<DocLevelQuery>,
     val lastRunContext: MutableMap<String, Any>,
@@ -14,5 +15,5 @@ data class IndexExecutionContext(
     val indexName: String,
     val concreteIndexName: String,
     val conflictingFields: List<String>,
-    val docIds: List<String>? = null
+    val docIds: List<String>? = null,
 )
