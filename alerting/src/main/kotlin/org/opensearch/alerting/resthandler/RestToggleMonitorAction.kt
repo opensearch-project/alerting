14148f6c11bd6187fe518d85ea262c3f2bfc49a9
vikhy-aws <191836418+vikhy-aws@users.noreply.github.com>
2025-01-22 11:39:58 -0800

    fix: change endpoint to match the code style for other APIs
    Signed-off-by: vikhy-aws <191836418+vikhy-aws@users.noreply.github.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/resthandler/RestToggleMonitorAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/resthandler/RestToggleMonitorAction.kt
@@ -42,15 +42,15 @@ class RestToggleMonitorAction : BaseRestHandler() {
         return mutableListOf(
             ReplacedRoute(
                 PUT,
-                "${AlertingPlugin.MONITOR_BASE_URI}/{monitorID}/enable",
+                "${AlertingPlugin.MONITOR_BASE_URI}/{monitorID}/_enable",
                 PUT,
-                "${AlertingPlugin.LEGACY_OPENDISTRO_MONITOR_BASE_URI}/{monitorID}/enable"
+                "${AlertingPlugin.LEGACY_OPENDISTRO_MONITOR_BASE_URI}/{monitorID}/_enable"
             ),
             ReplacedRoute(
                 PUT,
-                "${AlertingPlugin.MONITOR_BASE_URI}/{monitorID}/disable",
+                "${AlertingPlugin.MONITOR_BASE_URI}/{monitorID}/_disable",
                 PUT,
-                "${AlertingPlugin.LEGACY_OPENDISTRO_MONITOR_BASE_URI}/{monitorID}/disable"
+                "${AlertingPlugin.LEGACY_OPENDISTRO_MONITOR_BASE_URI}/{monitorID}/_disable"
             )
         )
     }
