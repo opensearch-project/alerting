80d94d6a9960631b56bc7d2b9c604d70eaa91b5b
Dennis Toepker <toepkerd@amazon.com>
2025-09-08 12:04:13 -0700

    MonitorV2 CRUD
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

---
+++ alerting/src/main/kotlin/org/opensearch/alerting/resthandler/RestGetMonitorV2Action.kt
@@ -0,0 +1,54 @@
+package org.opensearch.alerting.resthandler
+
+import org.apache.logging.log4j.LogManager
+import org.opensearch.alerting.AlertingPlugin
+import org.opensearch.alerting.util.context
+import org.opensearch.commons.alerting.action.AlertingActions
+import org.opensearch.commons.alerting.action.GetMonitorV2Request
+import org.opensearch.rest.BaseRestHandler
+import org.opensearch.rest.RestHandler.Route
+import org.opensearch.rest.RestRequest
+import org.opensearch.rest.RestRequest.Method.GET
+import org.opensearch.rest.RestRequest.Method.HEAD
+import org.opensearch.rest.action.RestActions
+import org.opensearch.rest.action.RestToXContentListener
+import org.opensearch.search.fetch.subphase.FetchSourceContext
+import org.opensearch.transport.client.node.NodeClient
+
+private val log = LogManager.getLogger(RestGetMonitorV2Action::class.java)
+
+class RestGetMonitorV2Action : BaseRestHandler() {
+
+    override fun getName(): String {
+        return "get_monitor_v2_action"
+    }
+
+    override fun routes(): List<Route> {
+        return mutableListOf(
+            Route(
+                GET,
+                "${AlertingPlugin.MONITOR_V2_BASE_URI}/{monitorV2Id}"
+            )
+        )
+    }
+
+    override fun prepareRequest(request: RestRequest, client: NodeClient): RestChannelConsumer {
+        log.debug("${request.method()} ${AlertingPlugin.MONITOR_V2_BASE_URI}/{monitorV2Id}")
+
+        val monitorV2Id = request.param("monitorV2Id")
+        if (monitorV2Id == null || monitorV2Id.isEmpty()) {
+            throw IllegalArgumentException("No MonitorV2 ID provided")
+        }
+
+        var srcContext = context(request)
+        if (request.method() == HEAD) {
+            srcContext = FetchSourceContext.DO_NOT_FETCH_SOURCE
+        }
+
+        val getMonitorV2Request = GetMonitorV2Request(monitorV2Id, RestActions.parseVersion(request), srcContext)
+        return RestChannelConsumer {
+                channel ->
+            client.execute(AlertingActions.GET_MONITOR_V2_ACTION_TYPE, getMonitorV2Request, RestToXContentListener(channel))
+        }
+    }
+}
