6e85a2f58b572ca470d705d347387b9deb28a57e
Dennis Toepker <toepkerd@amazon.com>
2025-09-10 15:36:53 -0700

    API Request/Response
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

---
+++ alerting/src/main/kotlin/org/opensearch/alerting/actionv2/DeleteMonitorV2Action.kt
@@ -0,0 +1,10 @@
+package org.opensearch.alerting.actionv2
+
+import org.opensearch.action.ActionType
+
+class DeleteMonitorV2Action private constructor() : ActionType<DeleteMonitorV2Response>(NAME, ::DeleteMonitorV2Response) {
+    companion object {
+        val INSTANCE = DeleteMonitorV2Action()
+        const val NAME = "cluster:admin/opensearch/alerting/v2/monitor/delete"
+    }
+}
