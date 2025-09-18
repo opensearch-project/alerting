782a43ea9413588593a27b2148dde4f9b4770dae
Mark Boyd <mark.boyd@gsa.gov>
2025-09-18 15:55:23 -0400

    fix expected schema versions in test
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- alerting/src/test/kotlin/org/opensearch/alerting/alerts/AlertIndicesIT.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/alerts/AlertIndicesIT.kt
@@ -54,7 +54,7 @@ class AlertIndicesIT : AlertingRestTestCase() {
 
         putAlertMappings(
             AlertIndices.alertMapping().trimStart('{').trimEnd('}')
-                .replace("\"schema_version\": 5", "\"schema_version\": 0")
+                .replace("\"schema_version\": 6", "\"schema_version\": 0")
         )
         assertIndexExists(AlertIndices.ALERT_INDEX)
         assertIndexExists(AlertIndices.ALERT_HISTORY_WRITE_INDEX)
@@ -64,9 +64,9 @@ class AlertIndicesIT : AlertingRestTestCase() {
         executeMonitor(createRandomMonitor())
         assertIndexExists(AlertIndices.ALERT_INDEX)
         assertIndexExists(AlertIndices.ALERT_HISTORY_WRITE_INDEX)
-        verifyIndexSchemaVersion(ScheduledJob.SCHEDULED_JOBS_INDEX, 8)
-        verifyIndexSchemaVersion(AlertIndices.ALERT_INDEX, 5)
-        verifyIndexSchemaVersion(AlertIndices.ALERT_HISTORY_WRITE_INDEX, 5)
+        verifyIndexSchemaVersion(ScheduledJob.SCHEDULED_JOBS_INDEX, 9)
+        verifyIndexSchemaVersion(AlertIndices.ALERT_INDEX, 6)
+        verifyIndexSchemaVersion(AlertIndices.ALERT_HISTORY_WRITE_INDEX, 6)
     }
 
     fun `test update finding index mapping with new schema version`() {
