2ebfb97cb65401ec301b77251552405b2faef281
Dennis Toepker <toepkerd@amazon.com>
2025-10-21 15:59:54 -0700

    PPL Alerting Models
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

---
+++ alerting/src/main/kotlin/org/opensearch/alerting/modelv2/PPLMonitorRunResult.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.modelv2
+
+import org.opensearch.alerting.modelv2.AlertV2.Companion.MONITOR_V2_NAME_FIELD
+import org.opensearch.alerting.modelv2.MonitorV2RunResult.Companion.ERROR_FIELD
+import org.opensearch.alerting.modelv2.MonitorV2RunResult.Companion.TRIGGER_RESULTS_FIELD
+import org.opensearch.core.common.io.stream.StreamInput
+import org.opensearch.core.common.io.stream.StreamOutput
+import org.opensearch.core.xcontent.ToXContent
+import org.opensearch.core.xcontent.XContentBuilder
+import java.io.IOException
+
+data class PPLMonitorRunResult(
+    override val monitorName: String,
+    override val error: Exception?,
+    override val triggerResults: Map<String, PPLTriggerRunResult>,
+    val pplQueryResults: Map<String, Map<String, Any?>> // key: trigger id, value: query results
+) : MonitorV2RunResult<PPLTriggerRunResult> {
+
+    @Throws(IOException::class)
+    @Suppress("UNCHECKED_CAST")
+    constructor(sin: StreamInput) : this(
+        sin.readString(), // monitorName
+        sin.readException(), // error
+        sin.readMap() as Map<String, PPLTriggerRunResult>, // triggerResults
+        sin.readMap() as Map<String, Map<String, Any?>> // pplQueryResults
+    )
+
+    override fun toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder {
+        builder.startObject()
+        builder.field(MONITOR_V2_NAME_FIELD, monitorName)
+        builder.field(ERROR_FIELD, error?.message)
+        builder.field(TRIGGER_RESULTS_FIELD, triggerResults)
+        builder.field(PPL_QUERY_RESULTS_FIELD, pplQueryResults)
+        builder.endObject()
+        return builder
+    }
+
+    @Throws(IOException::class)
+    override fun writeTo(out: StreamOutput) {
+        out.writeString(monitorName)
+        out.writeException(error)
+        out.writeMap(triggerResults)
+        out.writeMap(pplQueryResults)
+    }
+
+    companion object {
+        const val PPL_QUERY_RESULTS_FIELD = "ppl_query_results"
+    }
+}
---
+++ alerting/src/main/kotlin/org/opensearch/alerting/modelv2/PPLTriggerRunResult.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.modelv2
+
+import org.opensearch.alerting.modelv2.TriggerV2RunResult.Companion.ERROR_FIELD
+import org.opensearch.alerting.modelv2.TriggerV2RunResult.Companion.NAME_FIELD
+import org.opensearch.alerting.modelv2.TriggerV2RunResult.Companion.TRIGGERED_FIELD
+import org.opensearch.commons.alerting.model.QueryLevelTriggerRunResult
+import org.opensearch.commons.alerting.model.TriggerRunResult
+import org.opensearch.core.common.io.stream.StreamInput
+import org.opensearch.core.common.io.stream.StreamOutput
+import org.opensearch.core.xcontent.ToXContent
+import org.opensearch.core.xcontent.XContentBuilder
+import java.io.IOException
+
+data class PPLTriggerRunResult(
+    override var triggerName: String,
+    override var triggered: Boolean,
+    override var error: Exception?,
+) : TriggerV2RunResult {
+
+    @Throws(IOException::class)
+    @Suppress("UNCHECKED_CAST")
+    constructor(sin: StreamInput) : this(
+        triggerName = sin.readString(),
+        triggered = sin.readBoolean(),
+        error = sin.readException()
+    )
+
+    override fun toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder {
+        builder.startObject()
+        builder.field(NAME_FIELD, triggerName)
+        builder.field(TRIGGERED_FIELD, triggered)
+        builder.field(ERROR_FIELD, error?.message)
+        builder.endObject()
+        return builder
+    }
+
+    @Throws(IOException::class)
+    override fun writeTo(out: StreamOutput) {
+        out.writeString(triggerName)
+        out.writeBoolean(triggered)
+        out.writeException(error)
+    }
+
+    companion object {
+        @JvmStatic
+        @Throws(IOException::class)
+        fun readFrom(sin: StreamInput): TriggerRunResult {
+            return QueryLevelTriggerRunResult(sin)
+        }
+    }
+}
