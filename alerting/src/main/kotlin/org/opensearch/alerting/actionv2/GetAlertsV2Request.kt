d9614ba94390b28fa9581e743bfced6795a4403f
toepkerd <120457569+toepkerd@users.noreply.github.com>
2025-11-04 13:51:25 -0800

    PPL Alerting: Get Alerts and Alert Lifecycle (#1972)
    * PPL Alerting: Get Alerts and Alert Lifecycle
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * updating scheduled jobs schema version in alert indices IT
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * enabling alerting v2 by default
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * making get alerts response fields snake cased
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * removing unused get alerts params and adding serde tests for alertsv2 request and response
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * removing misleading comments
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * minor changes
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    * changed some setting names to go under alerting prefix, and optimizing alert expire logic to search for expired alerts in OS query instead of scanning Alerts for expiration in memory
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    
    ---------
    
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>
    Co-authored-by: Dennis Toepker <toepkerd@amazon.com>

---
+++ alerting/src/main/kotlin/org/opensearch/alerting/actionv2/GetAlertsV2Request.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.actionv2
+
+import org.opensearch.action.ActionRequest
+import org.opensearch.action.ActionRequestValidationException
+import org.opensearch.commons.alerting.model.Table
+import org.opensearch.core.common.io.stream.StreamInput
+import org.opensearch.core.common.io.stream.StreamOutput
+import java.io.IOException
+
+class GetAlertsV2Request : ActionRequest {
+    val table: Table
+    val severityLevel: String
+    val monitorV2Ids: List<String>?
+
+    constructor(
+        table: Table,
+        severityLevel: String,
+        monitorV2Ids: List<String>? = null,
+    ) : super() {
+        this.table = table
+        this.severityLevel = severityLevel
+        this.monitorV2Ids = monitorV2Ids
+    }
+
+    @Throws(IOException::class)
+    constructor(sin: StreamInput) : this(
+        table = Table.readFrom(sin),
+        severityLevel = sin.readString(),
+        monitorV2Ids = sin.readOptionalStringList(),
+    )
+
+    override fun validate(): ActionRequestValidationException? {
+        return null
+    }
+
+    @Throws(IOException::class)
+    override fun writeTo(out: StreamOutput) {
+        table.writeTo(out)
+        out.writeString(severityLevel)
+        out.writeOptionalStringCollection(monitorV2Ids)
+    }
+}
---
+++ alerting/src/main/kotlin/org/opensearch/alerting/resthandlerv2/RestGetAlertsV2Action.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.resthandlerv2
+
+import org.apache.logging.log4j.LogManager
+import org.opensearch.alerting.AlertingPlugin
+import org.opensearch.alerting.actionv2.GetAlertsV2Action
+import org.opensearch.alerting.actionv2.GetAlertsV2Request
+import org.opensearch.commons.alerting.model.Table
+import org.opensearch.rest.BaseRestHandler
+import org.opensearch.rest.RestHandler.Route
+import org.opensearch.rest.RestRequest
+import org.opensearch.rest.RestRequest.Method.GET
+import org.opensearch.rest.action.RestToXContentListener
+import org.opensearch.transport.client.node.NodeClient
+
+/**
+ * This class consists of the REST handler to retrieve alerts .
+ */
+class RestGetAlertsV2Action : BaseRestHandler() {
+
+    private val log = LogManager.getLogger(RestGetAlertsV2Action::class.java)
+
+    override fun getName(): String {
+        return "get_alerts_v2_action"
+    }
+
+    override fun routes(): List<Route> {
+        return listOf(
+            Route(
+                GET,
+                "${AlertingPlugin.MONITOR_V2_BASE_URI}/alerts"
+            )
+        )
+    }
+
+    override fun prepareRequest(request: RestRequest, client: NodeClient): RestChannelConsumer {
+        log.debug("${request.method()} ${AlertingPlugin.MONITOR_V2_BASE_URI}/alerts")
+
+        val sortString = request.param("sortString", "monitor_v2_name.keyword")
+        val sortOrder = request.param("sortOrder", "asc")
+        val missing: String? = request.param("missing")
+        val size = request.paramAsInt("size", 20)
+        val startIndex = request.paramAsInt("startIndex", 0)
+        val searchString = request.param("searchString", "")
+        val severityLevel = request.param("severityLevel", "ALL")
+        val monitorId: String? = request.param("monitorId")
+        val table = Table(
+            sortOrder,
+            sortString,
+            missing,
+            size,
+            startIndex,
+            searchString
+        )
+
+        val getAlertsV2Request = GetAlertsV2Request(
+            table,
+            severityLevel,
+            monitorId?.let { listOf(monitorId) }
+        )
+        return RestChannelConsumer {
+                channel ->
+            client.execute(GetAlertsV2Action.INSTANCE, getAlertsV2Request, RestToXContentListener(channel))
+        }
+    }
+}
