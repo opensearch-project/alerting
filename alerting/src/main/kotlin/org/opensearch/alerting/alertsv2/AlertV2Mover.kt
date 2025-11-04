77361e90f5e55afc13b75b9a4cdb1393d1dc09ae
Dennis Toepker <toepkerd@amazon.com>
2025-11-04 14:13:12 -0800

    misc changes
    Signed-off-by: Dennis Toepker <toepkerd@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/alertsv2/AlertV2Mover.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/alertsv2/AlertV2Mover.kt
@@ -206,9 +206,7 @@ class AlertV2Mover(
         // Explicitly specify that at least one should clause must match
         expiredAlertsBoolQuery.minimumShouldMatch(1)
 
-        // only alerts' monitor IDs should be fetched, the ID of the alert
-        // itself will be the document ID, which is part of the doc's metadata,
-        // not the doc's source, so it doesn't need to be fetched in the query
+        // search for the expired alerts
         val expiredAlertsSearchQuery = SearchSourceBuilder.searchSource()
             .query(expiredAlertsBoolQuery)
             .size(MAX_SEARCH_SIZE)
@@ -217,6 +215,8 @@ class AlertV2Mover(
             .source(expiredAlertsSearchQuery)
         val expiredAlertsResponse: SearchResponse = client.suspendUntil { search(expiredAlertsRequest, it) }
 
+        // parse the search results into full alert docs, as they will need to be
+        // indexed into alert history indices
         val expiredAlertV2s = mutableListOf<AlertV2>()
         expiredAlertsResponse.hits.forEach { hit ->
             expiredAlertV2s.add(
