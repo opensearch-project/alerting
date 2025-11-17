6ea9aa40da83fec4ad7d17f5b1085ac0d59286f2
Mark Boyd <mark.boyd@gsa.gov>
2025-11-17 17:00:20 -0500

    add test to prove that monitor results are removed from query-level trigger context
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

---
+++ alerting/src/test/kotlin/org/opensearch/alerting/script/QueryLevelTriggerExecutionContextTests.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.script
+
+import org.junit.Assert
+import org.opensearch.alerting.randomQueryLevelMonitor
+import org.opensearch.alerting.randomQueryLevelMonitorRunResult
+import org.opensearch.alerting.randomQueryLevelTrigger
+import org.opensearch.test.OpenSearchTestCase
+
+@Suppress("UNCHECKED_CAST")
+class QueryLevelTriggerExecutionContextTests : OpenSearchTestCase() {
+
+    fun `test results are excluded from query-level context`() {
+        val monitor = randomQueryLevelMonitor()
+        val trigger = randomQueryLevelTrigger()
+        val result = randomQueryLevelMonitorRunResult()
+        val context = QueryLevelTriggerExecutionContext(monitor, trigger, result)
+        val templateArgs = context.asTemplateArg()
+        Assert.assertFalse(templateArgs.containsKey("results"))
+    }
+}
