79b8215474738d6ca590366fc66b7d6a7b66f180
Mark Boyd <mark.boyd@gsa.gov>
2025-11-19 11:57:54 -0500

    fix test for results visibility in query level trigger execution contet
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- alerting/src/test/kotlin/org/opensearch/alerting/script/QueryLevelTriggerExecutionContextTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/script/QueryLevelTriggerExecutionContextTests.kt
@@ -10,6 +10,7 @@ import org.junit.Before
 import org.opensearch.alerting.randomQueryLevelMonitor
 import org.opensearch.alerting.randomQueryLevelMonitorRunResult
 import org.opensearch.alerting.randomQueryLevelTrigger
+import org.opensearch.alerting.settings.AlertingSettings
 import org.opensearch.common.settings.ClusterSettings
 import org.opensearch.common.settings.Setting
 import org.opensearch.common.settings.Settings
@@ -23,10 +24,11 @@ class QueryLevelTriggerExecutionContextTests : OpenSearchTestCase() {
     fun setup() {
         val settings = Settings.builder().build()
         val settingSet = hashSetOf<Setting<*>>()
+        settingSet.add(AlertingSettings.NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES)
         clusterSettings = ClusterSettings(settings, settingSet)
     }
 
-    fun `test results are excluded from query-level context`() {
+    fun `test results are excluded from query-level context when allowed roles is empty`() {
         val monitor = randomQueryLevelMonitor()
         val trigger = randomQueryLevelTrigger()
         val result = randomQueryLevelMonitorRunResult()
