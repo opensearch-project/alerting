31b076cd834cfff7c2b1ae6dd7bb50e5aba4b0c4
Mark Boyd <mark.boyd@gsa.gov>
2025-11-19 16:41:02 -0500

    separate tests into separate files
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- alerting/src/test/kotlin/org/opensearch/alerting/script/DocumentLevelTriggerExecutionContextTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/script/DocumentLevelTriggerExecutionContextTests.kt
@@ -6,9 +6,9 @@
 package org.opensearch.alerting.script
 
 import org.junit.Assert
-import org.opensearch.alerting.randomQueryLevelMonitor
-import org.opensearch.alerting.randomQueryLevelMonitorRunResult
-import org.opensearch.alerting.randomQueryLevelTrigger
+import org.opensearch.alerting.randomDocumentLevelMonitor
+import org.opensearch.alerting.randomDocumentLevelMonitorRunResult
+import org.opensearch.alerting.randomDocumentLevelTrigger
 import org.opensearch.alerting.randomUser
 import org.opensearch.alerting.settings.AlertingSettings
 import org.opensearch.common.settings.ClusterSettings
@@ -17,24 +17,24 @@ import org.opensearch.common.settings.Settings
 import org.opensearch.test.OpenSearchTestCase
 
 @Suppress("UNCHECKED_CAST")
-class QueryLevelTriggerExecutionContextTests : OpenSearchTestCase() {
+class DocumentLevelTriggerExecutionContextTests : OpenSearchTestCase() {
     private lateinit var clusterSettings: ClusterSettings
 
-    fun `test results are excluded from query-level context when allowed roles is empty`() {
+    fun `test results are excluded from document-level context when allowed roles is empty`() {
         val settings = Settings.builder().build()
         val settingSet = hashSetOf<Setting<*>>()
         settingSet.add(AlertingSettings.NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES)
         clusterSettings = ClusterSettings(settings, settingSet)
 
-        val monitor = randomQueryLevelMonitor()
-        val trigger = randomQueryLevelTrigger()
-        val result = randomQueryLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
+        val monitor = randomDocumentLevelMonitor()
+        val trigger = randomDocumentLevelTrigger()
+        val result = randomDocumentLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
         Assert.assertFalse(result.inputResults.results.isNullOrEmpty())
-        val context = QueryLevelTriggerExecutionContext(monitor, trigger, result, clusterSettings = clusterSettings)
+        val context = DocumentLevelTriggerExecutionContext(monitor, trigger, clusterSettings = clusterSettings)
         Assert.assertTrue(context.results.isNullOrEmpty())
     }
 
-    fun `test results are included in query-level context when allowed roles intersect monitor roles`() {
+    fun `test results are excluded in document-level context even when allowed roles intersect monitor roles`() {
         val settings = Settings
             .builder()
             .putList("plugins.alerting.notification_context_results_allowed_roles", listOf("role1", "role2"))
@@ -43,11 +43,11 @@ class QueryLevelTriggerExecutionContextTests : OpenSearchTestCase() {
         settingSet.add(AlertingSettings.NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES)
         clusterSettings = ClusterSettings(settings, settingSet)
 
-        val monitor = randomQueryLevelMonitor(user = randomUser(listOf("role1")))
-        val trigger = randomQueryLevelTrigger()
-        val result = randomQueryLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
+        val monitor = randomDocumentLevelMonitor(user = randomUser(listOf("role1")))
+        val trigger = randomDocumentLevelTrigger()
+        val result = randomDocumentLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
         Assert.assertFalse(result.inputResults.results.isNullOrEmpty())
-        val context = QueryLevelTriggerExecutionContext(monitor, trigger, result, clusterSettings = clusterSettings)
-        Assert.assertFalse(context.results.isNullOrEmpty())
+        val context = DocumentLevelTriggerExecutionContext(monitor, trigger, clusterSettings = clusterSettings)
+        Assert.assertTrue(context.results.isNullOrEmpty())
     }
 }
