de36934f0dfb48b40255bea7ae75c3089488b15e
Mark Boyd <mark.boyd@gsa.gov>
2025-11-20 17:31:22 -0500

    add test case for query-level context when NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES is not set
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- alerting/src/test/kotlin/org/opensearch/alerting/script/QueryLevelTriggerExecutionContextTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/script/QueryLevelTriggerExecutionContextTests.kt
@@ -20,13 +20,30 @@ import org.opensearch.test.OpenSearchTestCase
 class QueryLevelTriggerExecutionContextTests : OpenSearchTestCase() {
     private lateinit var clusterSettings: ClusterSettings
 
-    fun `test results are excluded from query-level context when allowed roles is empty`() {
+    fun `test results are excluded from query-level context when allowed roles are not set`() {
         val settings = Settings.builder().build()
         val settingSet = hashSetOf<Setting<*>>()
         settingSet.add(AlertingSettings.NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES)
         clusterSettings = ClusterSettings(settings, settingSet)
 
         val monitor = randomQueryLevelMonitor()
+        val trigger = randomQueryLevelTrigger()
+        val result = randomQueryLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
+        Assert.assertFalse(result.inputResults.results.isNullOrEmpty())
+        val context = QueryLevelTriggerExecutionContext(monitor, trigger, result, clusterSettings = clusterSettings)
+        Assert.assertFalse(context.results.isNullOrEmpty())
+    }
+
+    fun `test results are excluded from query-level context when allowed roles do not intersect monitor roles`() {
+        val settings = Settings
+            .builder()
+            .putList("plugins.alerting.notification_context_results_allowed_roles", listOf("role1"))
+            .build()
+        val settingSet = hashSetOf<Setting<*>>()
+        settingSet.add(AlertingSettings.NOTIFICATION_CONTEXT_RESULTS_ALLOWED_ROLES)
+        clusterSettings = ClusterSettings(settings, settingSet)
+
+        val monitor = randomQueryLevelMonitor(user = randomUser(listOf("role2")))
         val trigger = randomQueryLevelTrigger()
         val result = randomQueryLevelMonitorRunResult(listOf(mapOf("foo" to "bar")))
         Assert.assertFalse(result.inputResults.results.isNullOrEmpty())
