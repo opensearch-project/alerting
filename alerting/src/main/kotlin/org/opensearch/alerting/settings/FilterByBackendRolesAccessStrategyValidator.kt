092be2357781aa79998dc0c58ecfa6b104044c36
Mark Boyd <mark.boyd@gsa.gov>
2026-03-06 17:35:43 -0500

    use enum and validator for filter_by_backend_roles_access_strategy setting
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

---
+++ alerting/src/main/kotlin/org/opensearch/alerting/settings/FilterByBackendRolesAccessStrategyValidator.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.alerting.settings
+
+import org.opensearch.common.settings.Setting
+
+class FilterByBackendRolesAccessStrategyValidator : Setting.Validator<String> {
+    override fun validate(strategy: String) {
+        val allStrategies: List<String> = FilterByBackendRolesAccessStrategy.entries.map { it.strategy }
+
+        when (strategy) {
+            FilterByBackendRolesAccessStrategy.INTERSECT.strategy,
+            FilterByBackendRolesAccessStrategy.ALL.strategy -> {}
+            else -> throw IllegalArgumentException(
+                "Setting value must be one of [$allStrategies]"
+            )
+        }
+    }
+}
