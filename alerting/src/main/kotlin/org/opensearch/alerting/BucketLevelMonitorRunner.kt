8ad1ffc03941af7a2d9e2347329be6a463abce74
Surya Sashank Nistala <snistala@amazon.com>
2026-05-18 19:41:45 +0000

    fix: Handle typed_keys prefix and remote target in bucket-level monitors
    - Skip local index alias resolution when monitor target is remote
      (InputService.resolveOnlyQueryableIndicesFromLocalClusterAliases NPEs
      for remote targets that don't exist locally)
    - Handle typed_keys prefix in TriggerService agg key lookup (endsWith
      fallback for keys like 'sterms#by_message')
    - Safe access on currentAlerts map to avoid NPE when alerts index is
      empty or newly created
    
    Signed-off-by: Surya Sashank Nistala <snistala@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/BucketLevelMonitorRunner.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/BucketLevelMonitorRunner.kt
@@ -166,7 +166,7 @@ object BucketLevelMonitorRunner : MonitorRunner() {
 
             for (trigger in monitor.triggers) {
                 // The currentAlerts map is formed by iterating over the Monitor's Triggers as keys so null should not be returned here
-                val currentAlertsForTrigger = currentAlerts[trigger]!!
+                val currentAlertsForTrigger = currentAlerts[trigger] ?: mutableMapOf()
                 val triggerCtx = BucketLevelTriggerExecutionContext(
                     monitor, trigger as BucketLevelTrigger, monitorResult,
                     clusterSettings = monitorCtx.clusterService!!.clusterSettings
