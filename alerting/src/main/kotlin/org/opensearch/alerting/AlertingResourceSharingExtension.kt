2576e6b7078141b942e91bf855c6303a2beb79c6
Darshit Chanpura <dchanp@amazon.com>
2026-06-20 15:08:40 -0700

    Fix resource types
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/AlertingResourceSharingExtension.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/AlertingResourceSharingExtension.kt
@@ -5,8 +5,6 @@
 
 package org.opensearch.alerting
 
-import org.opensearch.alerting.alerts.AlertIndices.Companion.ALL_ALERT_INDEX_PATTERN
-import org.opensearch.alerting.comments.CommentsIndices.Companion.ALL_COMMENTS_INDEX_PATTERN
 import org.opensearch.commons.alerting.model.ScheduledJob.Companion.SCHEDULED_JOBS_INDEX
 import org.opensearch.security.spi.resources.ResourceProvider
 import org.opensearch.security.spi.resources.ResourceSharingExtension
@@ -18,14 +16,6 @@ class AlertingResourceSharingExtension : ResourceSharingExtension {
             object : ResourceProvider {
                 override fun resourceType(): String = "monitor"
                 override fun resourceIndexName(): String = SCHEDULED_JOBS_INDEX
-            },
-            object : ResourceProvider {
-                override fun resourceType(): String = "alert"
-                override fun resourceIndexName(): String = ALL_ALERT_INDEX_PATTERN
-            },
-            object : ResourceProvider {
-                override fun resourceType(): String = "comment"
-                override fun resourceIndexName(): String = ALL_COMMENTS_INDEX_PATTERN
             }
         )
     }
--- alerting/src/test/kotlin/org/opensearch/alerting/AlertingResourceSharingExtensionTests.kt
+++ alerting/src/test/kotlin/org/opensearch/alerting/AlertingResourceSharingExtensionTests.kt
@@ -7,8 +7,6 @@ package org.opensearch.alerting
 
 import org.junit.Before
 import org.mockito.Mockito.mock
-import org.opensearch.alerting.alerts.AlertIndices
-import org.opensearch.alerting.comments.CommentsIndices
 import org.opensearch.commons.alerting.model.ScheduledJob
 import org.opensearch.security.spi.resources.client.ResourceSharingClient
 import org.opensearch.test.OpenSearchTestCase
@@ -23,27 +21,15 @@ class AlertingResourceSharingExtensionTests : OpenSearchTestCase() {
         ResourceSharingClientAccessor.clear()
     }
 
-    fun `test getResourceProviders returns three providers`() {
+    fun `test getResourceProviders returns one provider`() {
         val providers = extension.getResourceProviders()
-        assertEquals(3, providers.size)
+        assertEquals(1, providers.size)
     }
 
     fun `test monitor provider has correct type and index`() {
         val providers = extension.getResourceProviders()
         val monitorProvider = providers.first { it.resourceType() == "monitor" }
         assertEquals(ScheduledJob.SCHEDULED_JOBS_INDEX, monitorProvider.resourceIndexName())
-    }
-
-    fun `test alert provider has correct type and index`() {
-        val providers = extension.getResourceProviders()
-        val alertProvider = providers.first { it.resourceType() == "alert" }
-        assertEquals(AlertIndices.ALL_ALERT_INDEX_PATTERN, alertProvider.resourceIndexName())
-    }
-
-    fun `test comment provider has correct type and index`() {
-        val providers = extension.getResourceProviders()
-        val commentProvider = providers.first { it.resourceType() == "comment" }
-        assertEquals(CommentsIndices.ALL_COMMENTS_INDEX_PATTERN, commentProvider.resourceIndexName())
     }
 
     fun `test assignResourceSharingClient sets client in accessor`() {
